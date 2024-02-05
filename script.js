cd41ba8e49a2349cb4247d5af9b67c1c9cf99bf9
Jeriko-jk <156047260+Jeriko-jk@users.noreply.github.com>
2024-02-05 21:22:06 +0700

    Update

--- script.js
+++ script.js
@@ -38,11 +38,31 @@ function validateEmail(){
     var email = document.getElementById('contact-email').value;
     if(email.length == 0){
         emailError.innerHTML = 'email is required'
+        return false;
     }
     if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
-        emailError.innerHTML = 'email is required'
+        emailError.innerHTML = 'email invalid'
         return false;
     }
     emailError.innerHTML = '<i class="fa-solid fa-check"></i>';
     return true;
+}
+function validateMessage(){
+    var message =document.getElementById('contact-message').value;
+    var required = 30;
+    var left =required -message.length;
+    if(left>0){
+        messageError.innerHTML = left+'More char required';
+        return false;
+    }
+    messageError.innerHTML='<i class="fa-solid fa-check"></i>';
+    return true;
+}
+function validateForm(){
+    if(!validateName()|| !validatePhone()||!validateEmail()|| !validateMessage()){
+        submitError.style.display='block';
+        submitError.innerHTML='please fix error to subit'
+        setTimeout(function(){submitError.style.display='none';},3000)
+        return false;
+    }
 }
